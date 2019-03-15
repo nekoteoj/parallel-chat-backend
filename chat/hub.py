a9480114e6a2b73b5a2a1edb8d703133e485a7e2
schwan <schwan46494@gmail.com>
2019-03-15 16:31:37 +0700

    add create group

--- chat/hub.py
+++ chat/hub.py
@@ -2,7 +2,7 @@ from socketio import Server
 from .db import get_db
 from . import utils
 import json
-
+from bson.objectid import ObjectId
 def init(sio: Server):
     @sio.on('connect')
     def connect_sio(sid, message):
@@ -21,4 +21,46 @@ def init(sio: Server):
         if not name_search:
             posts.insert_one(json_message).inserted_id
             name_search = posts.find_one(json_message)
-        sio.emit('name', utils.query_dict(name_search), room=sid)
+            sio.emit('created', utils.query_dict(name_search), room = sid)
+        else:
+            sio.emit('name', utils.query_dict(name_search), room = sid)
+
+    @sio.on('create_group')
+    def add_group_sio(sid, message):
+        print(message)
+        db = get_db()
+        json_message = json.loads(message)
+        posts_user = db.User   
+        name_search = posts_user.find_one({"username" : json_message["username"]})
+        print(name_search)
+        if not name_search:
+            sio.emit('Error-name_not_found', None, room=sid)
+        else:
+            posts_group = db.Group
+            group_search = posts_group.find_one({"group_name" : json_message["group_name"]})
+            if not group_search:
+                group_message =     {
+                                        "group_name" : json_message["group_name"],
+                                        "user" : 
+                                            [ 
+                                                { 
+                                                    "name_ID" : str(name_search['_id']),
+                                                    "last_read" : utils.get_current_time()
+                                                }
+                                            ]
+                                    }
+                print(json.dumps(group_message))
+                posts_group.insert_one(group_message).inserted_id
+                group_val = posts_group.find_one({"group_name" : json_message["group_name"]})
+                sio.emit('group_created', utils.query_dict(group_val), room=sid)
+
+            else:
+                sio.emit('Error-group_already_created', None, room=sid)
+
+
+
+
+        # else:
+        #     posts_user.insert_one(json_message).inserted_id
+        #     name_search = posts_user.find_one(json_message)
+
--- chat/utils.py
+++ chat/utils.py
@@ -1,3 +1,6 @@
+import datetime
+def get_current_time():
+    return datetime.datetime.now().isoformat()
 def query_dict(query):
     q = query.copy()
     q['_id'] = str(q['_id'])
