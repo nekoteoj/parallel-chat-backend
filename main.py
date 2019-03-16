a71dd46213f02261856bc59e18b368e15731985f
Pisit Wajanasara <compisit1234@gmail.com>
2019-03-16 21:33:52 +0700

    Add peer support using Radis

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from flask import Flask, jsonify
 import socketio
-from gevent import pywsgi
+from gevent import pywsgi, monkey
 from geventwebsocket.handler import WebSocketHandler
 import os
+
+monkey.patch_all()
 
 from chat import hub, route
 
 async_mode = 'gevent'
 
-PORT = os.environ.get('PORT', 3000)
+PORT = int(os.environ.get('PORT', 3000))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'schwanfc')
+PEER = os.environ.get('PEER', 'false').lower() == 'true'
 
-sio = socketio.Server(logger=True, async_mode=async_mode)
+if PEER:
+    print('Running in peer mode!')
+    mgr = socketio.RedisManager('redis://')
+    sio = socketio.Server(client_manager=mgr, logger=True, async_mode=async_mode)
+else:
+    sio = socketio.Server(logger=True, async_mode=async_mode)
 app = Flask(__name__)
 app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
 app.config['SECRET_KEY'] = SECRET_KEY
